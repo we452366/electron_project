aa2d5cff890ccb10c3ff9b591a4413f8652876f6
wildwood <lihq151@chinaunicom.cn>
2020-11-19 19:36:38 +0800

    feat: 新增控制面窗口

---
+++ app/main/control/index.js
@@ -0,0 +1,19 @@
+const { BrowserWindow } = require('electron');
+const path = require('path');
+
+let control = null;
+
+const create = () => {
+    control = new BrowserWindow({
+        width: 800,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    control.loadFile(path.resolve(__dirname, '../../render/static/control.html'))
+};
+
+module.exports = {
+    create
+}
